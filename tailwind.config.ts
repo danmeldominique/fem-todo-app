b302ed7b732a6f7efb3ce6718c9717a9d3a1fcc5
Daniel Dominique <danmeldominique@gmail.com>
2023-09-10 20:47:21 +1000

    Project init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,37 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
+  darkMode: 'class',
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ['var(--josefin-sans)', 'sans-serif'],
       },
+      colors: {
+        // Primary
+        Primary: 'hsl(220, 98%, 61%)',
+        // Light theme
+        L_VeryLightGray: 'hsl(0, 0%, 98%)',
+        L_VeryLightGrayishBlue: 'hsl(236, 33%, 92%)',
+        L_LightGrayishBlue: 'hsl(233, 11%, 84%)',
+        L_DarkGrayishBlue: 'hsl(236, 9%, 61%)',
+        L_VeryDarkGrayishBlue: 'hsl(235, 19%, 35%)',
+        // Dark Mode
+        D_VeryDarkBlue: 'hsl(235, 21%, 11%)',
+        D_VeryDarkDesaturatedBlue: 'hsl(235, 24%, 19%)',
+        D_LightGrayishBlue: 'hsl(234, 39%, 85%)',
+        D_LightGrayishBlueHover: 'hsl(236, 33%, 92%)',
+        D_DarkGrayishBlue: 'hsl(234, 11%, 52%)',
+        D_VeryDarkGrayishBlue: 'hsl(233, 14%, 35%)',
+        D_VeryDarkGrayishBlueDarker: 'hsl(233, 14%, 35%)',
+        // Check mark
+        CheckFrom: 'hsl(192, 100%, 67%)',
+        CheckTo: 'hsl(280, 87%, 65%)',
+      }
     },
   },
   plugins: [],
